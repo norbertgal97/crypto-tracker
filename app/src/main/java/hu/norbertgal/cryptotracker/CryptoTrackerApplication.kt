fc7c438da18ff369832a891d635d49bb6b518ba0
Gál Norbert <norbert.gal97@gmail.com>
2021-04-16 15:30:34 +0200

    prepare unit tests

--- app/src/main/java/hu/norbertgal/cryptotracker/CryptoTrackerApplication.kt
+++ app/src/main/java/hu/norbertgal/cryptotracker/CryptoTrackerApplication.kt
@@ -1,8 +1,10 @@
 package hu.norbertgal.cryptotracker
 
 import android.app.Application
+import android.content.Context
 import hu.norbertgal.cryptotracker.interactor.InteractorModule
 import hu.norbertgal.cryptotracker.repository.network.NetworkModule
+import okhttp3.internal.Internal.instance
 
 class CryptoTrackerApplication: Application() {
     lateinit var injector: CryptoTrackerApplicationComponent
